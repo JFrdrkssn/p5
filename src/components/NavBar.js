586590ea47611515378fe5099d4b12f0a1673b73
JFrdrkssn <johanf9401@gmail.com>
2022-05-17 14:21:18 +0000

    Replace Bootstrap Nav.Link with react-router-dom NavLink for URL/router handling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,23 +2,26 @@ import React from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import logo from "../assets/llama-logo.png";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand className={styles.Brand}>
-          <img src={logo} alt="logo" height="55" />
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand className={styles.Brand}>
+            <img src={logo} alt="logo" height="55" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
+            <NavLink to="/login">
               <i className="fas fa-sign-in-alt"></i>Log in
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink to="/signup">
               <i className="fas fa-user-plus"></i>Sign up
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
