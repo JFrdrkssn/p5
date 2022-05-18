f6fbf6b5993bc025f00adf99cb63c1ecf548c27d
JFrdrkssn <johanf9401@gmail.com>
2022-05-18 11:25:23 +0000

    Add user context data and conditional rendering in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import logo from "../assets/logo-llama.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../App";
 
 const NavBar = () => {
+  // Variable to display current user in navbar
+  const currentUser = useContext(CurrentUserContext);
+
+  // Icons visible to non-logged in users
+  const loggedOutIcons = (
+    <>
+      {" "}
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/login">
+        <i className="fas fa-sign-in-alt"></i>Log in
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signup">
+        <i className="fas fa-user-plus"></i>Sign up
+      </NavLink>
+    </>
+  );
+
+  // Icons visible to logged in users
+  const loggedInIcons = <>{currentUser?.username}</>;
+
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
@@ -17,16 +43,10 @@ const NavBar = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink
+              to="/"
               className={styles.NavLink}
-              activeClassName={styles.Active}
-              to="/login">
-              <i className="fas fa-sign-in-alt"></i>Log in
-            </NavLink>
-            <NavLink
-              className={styles.NavLink}
-              activeClassName={styles.Active}
-              to="/signup">
-              <i className="fas fa-user-plus"></i>Sign up
+              activeClassName={styles.Active}>
+              {currentUser ? loggedInIcons : loggedOutIcons}
             </NavLink>
           </Nav>
         </Navbar.Collapse>
