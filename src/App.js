8a1e340bf333976c89e4a07fc88448a71a91467a
JFrdrkssn <johanf9401@gmail.com>
2022-05-18 11:44:29 +0000

    Refactor user ccontext data code to CurrentUserContext.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { createContext, useEffect, useState } from "react";
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
@@ -6,46 +5,20 @@ import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import LogInForm from "./pages/auth/LogInForm";
-import axios from "axios";
-
-export const CurrentUserContext = createContext();
-export const SetCurrentUserContext = createContext();
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null);
-
-  const handleMount = async () => {
-    try {
-      const { data } = await axios.get("dj-rest-auth/user/");
-      setCurrentUser(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  // call handleMount when components mount
-  useEffect(() => {
-    handleMount();
-  }, []);
-
-  // Get current user and function to update current user.
-  // Child components have access to context object, i.e user.
   return (
-    <CurrentUserContext.Provider value={currentUser}>
-      <SetCurrentUserContext.Provider value={setCurrentUser}>
-        <div className={styles.App}>
-          <NavBar />
-          <Container className={styles.Main}>
-            <Switch>
-              <Route exact path="/" render={() => <h1>Home page</h1>} />
-              <Route exact path="/login" render={() => <LogInForm />} />
-              <Route exact path="/signup" render={() => <SignUpForm />} />
-              <Route render={() => <p>How did you get here?</p>} />
-            </Switch>
-          </Container>
-        </div>
-      </SetCurrentUserContext.Provider>
-    </CurrentUserContext.Provider>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/login" render={() => <LogInForm />} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route render={() => <p>How did you get here?</p>} />
+        </Switch>
+      </Container>
+    </div>
   );
 }
 
