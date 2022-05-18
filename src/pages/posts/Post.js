14776de2c1bd0ed01e20f9a0a01dd4560e2a63dd
JFrdrkssn <johanf9401@gmail.com>
2022-05-18 23:17:37 +0000

    Add Post component, displays profile image and username with link to profile

---
+++ src/pages/posts/Post.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styles from "../../styles/Post.module.css";
+import appStyles from "../../App.module.css";
+import navStyles from "../../styles/NavBar.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Card, Media } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+
+const Post = (props) => {
+  const {
+    id,
+    owner,
+    profile_id,
+    profile_image,
+    updated_at,
+    title,
+    content,
+    image,
+    likes_count,
+    like_id,
+    comments_count,
+  } = props;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  return (
+    <Card className={appStyles.Content}>
+      <Card.Body>
+        <Media className="align-items-center justify-content-between">
+          <Link className={navStyles.NavLink} to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={55} />
+            {owner}
+          </Link>
+          <div>
+            <span>{updated_at}</span>
+          </div>
+        </Media>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Post;
