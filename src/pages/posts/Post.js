b78cebc5569fc74ce6d571fa5a9d2b748582ba10
JFrdrkssn <johanf9401@gmail.com>
2022-05-18 23:27:46 +0000

    Display post image, title and content on posts

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -20,23 +20,32 @@ const Post = (props) => {
     likes_count,
     like_id,
     comments_count,
+    postPage,
   } = props;
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
 
   return (
-    <Card className={appStyles.Content}>
+    <Card className={`${appStyles.Content} mb-2`}>
       <Card.Body>
         <Media className="align-items-center justify-content-between">
           <Link className={navStyles.NavLink} to={`/profiles/${profile_id}`}>
             <Avatar src={profile_image} height={55} />
             {owner}
           </Link>
-          <div>
+          <div className="d-flex align-items-center">
             <span>{updated_at}</span>
+            {is_owner && postPage && "..."}
           </div>
         </Media>
+      </Card.Body>
+      <Link to={`/posts/${id}`}>
+        <Card.Img src={image} alt={title} />
+      </Link>
+      <Card.Body>
+        {title && <Card.Title className="text-center">{title}</Card.Title>}
+        {content && <Card.Text>{content}</Card.Text>}
       </Card.Body>
     </Card>
   );
