c903302c59fab9e0db85a1cf48c7d5224a92f1aa
JFrdrkssn <johanf9401@gmail.com>
2022-05-18 11:10:07 +0000

    Add log in form with API, user data and error handling

---
+++ src/pages/auth/LogInForm.js
@@ -0,0 +1,109 @@
+import React, { useContext, useState } from "react";
+import { Form, Alert, Container, Row, Col, Button } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import axios from "axios";
+import styles from "../../styles/LogInSignUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import appStyles from "../../App.module.css";
+import { SetCurrentUserContext } from "../../App";
+
+function LogInForm() {
+  const [logInData, setLogInData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const setCurrentUser = useContext(SetCurrentUserContext);
+
+  const { username, password } = logInData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  const handleChange = (event) => {
+    setLogInData({
+      ...logInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  /* Handles submitting form data when loggin in to the site.
+    Request user data from API.
+    Redirects to home page.
+  */
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axios.post("/dj-rest-auth/login/", logInData);
+      setCurrentUser(data.user);
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  return (
+    <Row className={styles.Row}>
+      <Col className="my-auto p-0 p-md-2 offset-3" md={6}>
+        <Container className={`${appStyles.Content} p-4 `}>
+          <h1 className={styles.Header}>log in</h1>
+
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="text"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx} className="text-center">
+                {message}
+              </Alert>
+            ))}
+
+            <Form.Group controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                className={styles.Input}
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+            {errors.password?.map((message, idx) => (
+              <Alert variant="warning" key={idx} className="text-center">
+                {message}
+              </Alert>
+            ))}
+
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Bright} ${btnStyles.Wide}`}
+              type="submit">
+              LOG IN
+            </Button>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3 text-center">
+                {message}
+              </Alert>
+            ))}
+          </Form>
+        </Container>
+        <Container className={`mt-3 ${appStyles.Content}`}>
+          <Link className={styles.Link} to="/signup">
+            <span>Sign up</span> if you don't have an account.
+          </Link>
+        </Container>
+      </Col>
+    </Row>
+  );
+}
+
+export default LogInForm;
