2c1a261db2ca1fcb2b581bdfed07df489fe0e914
JFrdrkssn <johanf9401@gmail.com>
2022-05-18 11:42:09 +0000

    Add user context data with API and error handling for import in other files

---
+++ src/context/CurrentUserContext.js
@@ -0,0 +1,36 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
+
+export const useCurrentUser = () => useContext(CurrentUserContext);
+export const useSetCurrentUser = () => useContext(SetCurrentUserContext);
+
+export const CurrentUserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const handleMount = async () => {
+    try {
+      const { data } = await axios.get("dj-rest-auth/user/");
+      setCurrentUser(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // call handleMount when components mount
+  useEffect(() => {
+    handleMount();
+  }, []);
+
+  return (
+    // Get current user and function to update current user.
+    // Child components have access to context object, i.e user.
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        {children}
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
+  );
+};
