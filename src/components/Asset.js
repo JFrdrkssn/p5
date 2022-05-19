aeaa1019457ce6b94f4e1b565d1998f7a0a3fc74
JFrdrkssn <johanf9401@gmail.com>
2022-05-19 18:24:41 +0000

    Change color and animation type of Bootstrap Spinner and add ARIA for accessibility

--- src/components/Asset.js
+++ src/components/Asset.js
@@ -5,7 +5,11 @@ import styles from "../styles/Asset.module.css";
 const Asset = ({ spinner, src, message }) => {
   return (
     <div className={`${styles.Asset} p-4`}>
-      {spinner && <Spinner animation="border" />}
+      {spinner && (
+        <Spinner animation="grow" variant="light" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      )}
       {src && <img src={src} alt={message} />}
       {message && <p className="mt-4">{message}</p>}
     </div>
