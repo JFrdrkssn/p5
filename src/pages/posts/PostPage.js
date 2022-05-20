841b2181e5ee234bb5c99387bf35407d2a1bd8a4
JFrdrkssn <johanf9401@gmail.com>
2022-05-20 18:33:33 +0000

    Add CommentCreateForm component for logged in users on post detail views

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Col, Row, Container } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import appStyles from "../../App.module.css";
 import Post from "./Post";
 
@@ -10,6 +12,9 @@ function PostPage() {
 
   // Set initial state to object that contains an empty array of results.
   const [post, setPost] = useState({ results: [] });
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
@@ -32,7 +37,19 @@ function PostPage() {
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
         <Post {...post.results[0]} setPosts={setPost} postPage />
-        <Container className={appStyles.Content}>Comments</Container>
+        <Container className={appStyles.Content}>
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+        </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles for desktop
