fe39292695a64c7283130167bb612863fd5240fd
JFrdrkssn <johanf9401@gmail.com>
2022-05-21 15:15:58 +0000

    Add PopularPosts component to display the most commented posts on the site

---
+++ src/pages/posts/PopularPosts.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+import styles from "../../styles/PopularCards.module.css";
+import navStyles from "../../styles/NavBar.module.css";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Asset from "../../components/Asset";
+
+const PopularPosts = (props) => {
+  const [postsData, setPostsData] = useState({
+    pagePosts: { results: [] },
+    popularPosts: { results: [] },
+  });
+  const { popularPosts } = postsData;
+  const currentUser = useCurrentUser();
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get("/posts/?ordering=-comments_count");
+        setPostsData((prevState) => ({
+          ...prevState,
+          popularPosts: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
+
+  return (
+    <Container className={styles.PopularCard}>
+      {popularPosts.results.length ? (
+        <>
+          <p>Top commented posts</p>
+          {popularPosts.results.map((post) => (
+            <>
+              <Link className={navStyles.NavLink} to={`/posts/${post.id}`}>
+                <Avatar src={post.profile_image} height={20} />
+                <span key={post.id}>{post.title}</span>
+              </Link>
+              <span className={styles.Count}>{post.comments_count}</span>
+              <br />
+            </>
+          ))}
+        </>
+      ) : (
+        <Asset spinner />
+      )}
+    </Container>
+  );
+};
+
+export default PopularPosts;
