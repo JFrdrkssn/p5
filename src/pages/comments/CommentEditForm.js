8e04465fab498a733bb0b87f76cdecc4ed7f7cba
JFrdrkssn <johanf9401@gmail.com>
2022-05-21 12:48:09 +0000

    Add CommentEdit form for editing of owned comments

---
+++ src/pages/comments/CommentEditForm.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import { axiosRes } from "../../api/axiosDefaults";
+import styles from "../../styles/CommentCreateEditForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
+
+function CommentEditForm(props) {
+  const { id, content, setShowEditForm, setComments } = props;
+
+  const [formContent, setFormContent] = useState(content);
+
+  const handleChange = (event) => {
+    setFormContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.put(`/comments/${id}/`, {
+        content: formContent.trim(),
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.map((comment) => {
+          return comment.id === id
+            ? {
+                ...comment,
+                content: formContent.trim(),
+                updated_at: "now",
+              }
+            : comment;
+        }),
+      }));
+      setShowEditForm(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="pr-1 mb-2">
+        <Form.Control
+          className={styles.Input}
+          as="textarea"
+          value={formContent}
+          onChange={handleChange}
+          rows={2}
+        />
+      </Form.Group>
+      <div className="text-right">
+        <button
+          className={`${btnStyles.Button} ${btnStyles.Bright}`}
+          disabled={!content.trim()}
+          type="submit">
+          Save
+        </button>
+        <button
+          className={btnStyles.Button}
+          onClick={() => setShowEditForm(false)}
+          type="button">
+          Cancel
+        </button>
+      </div>
+    </Form>
+  );
+}
+
+export default CommentEditForm;
